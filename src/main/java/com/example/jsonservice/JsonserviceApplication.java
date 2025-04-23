de5266c75dfa06052683df5742b346f24d43b179
edensiboni <edensiboni5@gmail.com>
2025-04-23 22:24:43 +0300

    First commit

---
+++ src/main/java/com/example/jsonservice/JsonserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jsonservice/JsonserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
